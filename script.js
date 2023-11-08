73ad1bbaca240a699ef04101cdad16ad44387907
evanderzzstudio <evanderstudio@gmail.com>
2023-11-08 18:00:00 +0700

    The commit message involves removing unnecessary code and adding quiz questions and answers.

---
+++ script.js
@@ -0,0 +1,41 @@
+const question = [
+    {
+        question: "Which is large animal in the world",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Blue whale", correct: true },
+            { text: "Elephant", correct: false },
+            { text: "Giraffe", correct: false },
+        ]
+    },
+    {
+        question: "What is the capital of France",
+        answers: [
+            { text: "Berlin", correct: false },
+            { text: "Madrid", correct: false },
+            { text: "Rome", correct: false },
+            { text: "Paris", correct: true },
+        ]
+    },
+    {
+        question: "What is the largest planet in our solar system",
+        answers: [
+            { text: "Mars", correct: false },
+            { text: "Jupiter", correct: true },
+            { text: "Venus", correct: false },
+            { text: "Saturn", correct: false },
+        ]
+    },
+    {
+        question: "What is the most widely spoken language in the world",
+        answers: [
+            { text: "French", correct: false },
+            { text: "Blue Spanish", correct: false },
+            { text: "English", correct: true },
+            { text: "German", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const questionElement = document.getElementById("answer-buttons");
